62f2813bf317019b9108a40578ce1f8db1f48245
Jevgeni <67290830+Jevgeni-i-am@users.noreply.github.com>
2022-12-09 22:32:43 +0200

    Create kirjanpito.py

---
+++ kirjanpito.py
@@ -0,0 +1,32 @@
+def nayta_UI():
+    """Näyttää käyttöliittymän. Alkuvalikko"""
+
+
+def alkusaldo():
+    """Lataa alkusaldon"""
+
+
+def tallennus():
+    """Tiedot pitää pystyä tallentamaan tiedostoon ja latamaan sieltä."""
+
+
+def saldon_korjaus(saldo: int) -> int:
+    """Käyttäjä pystyy syöttämään alkusaldonsa sekä Sovellukseen pystyy lisäämään vastaavasti tuloja, joista kirjataan samat asiat."""
+
+
+def nayta_suurempi_kuin(suurempi: int) -> dict:
+    """Sovelluksella pystyy etsimään menoja tai tuloja, jotka ylittävät annetun summan
+(käyttäjä voisi esimerkiksi haluta näyttää kaikki menot ja tulot, jotka ovat suurempia kuin 25 euroa)."""
+
+
+def menoeran_lisays(nimi: str, summa: int):
+    """Sovellukseen pystyy lisäämään menoja, joista kirjataan tapahtuma ja summa."""
+
+
+def etsi_menoeran(hakutermi: str) -> str:
+    """Sovelluksella pystyy etsimään menoja tai tuloja tietyn hakusanan mukaan.
+"""
+
+
+def listaa_tapahtumat() -> str:
+    """Sovellus osaa näyttää kaikki menot ja tulot ja saldon näiden jälkeen(huomioiden alkusaldon)."""
